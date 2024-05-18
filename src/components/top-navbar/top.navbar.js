3bea493c7f5e7e53a9f75d5dedf57af5ac46afd9
Azharul  Islam <2014751037@uits.edu.bd>
2024-05-19 01:21:15 +0600

    Update Topbar

--- src/components/top-navbar/top.navbar.js
+++ src/components/top-navbar/top.navbar.js
@@ -6,75 +6,89 @@ function TopNavbar() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     return (
-        <nav
-            class="navbar navbar-expand-lg navbar-light bg-light"
-            style={{ height: "70px" }}>
-            <div class="container-fluid">
-                <h3 class="navbar-brand" style={{ marginLeft: "5%" }}>
-                    TaskTrek
-                </h3>
-
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand ms-5" href="#">
+                    <span
+                        style={{
+                            fontWeight: "bold",
+                            fontSize: "2rem",
+                            letterSpacing: "2px",
+                            // marginLeft: "100px",
+                        }}>
+                        TaskTrek
+                    </span>
+                </span>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div
-                    class="collapse navbar-collapse"
+                    className="collapse navbar-collapse justify-content-end"
                     id="navbarSupportedContent">
-                    <form class="d-flex" style={{ marginLeft: "70%" }}>
+                    <form className="d-flex me-3">
                         <input
-                            class="form-control me-2"
+                            className="form-control me-2"
                             type="search"
                             placeholder="Search"
                             aria-label="Search"
                         />
-                        <button class="btn btn-outline-success" type="submit">
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit">
                             Search
                         </button>
                     </form>
-
-                    <div
-                        class=" navbar-nav dropdown"
-                        style={{ marginLeft: "2%" }}>
-                        <span
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            id="navbarDropdown"
-                            role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            <i
-                                class="bi bi-person-circle"
-                                style={{ fontSize: "2em" }}></i>
-                        </span>
-
-                        <ul
-                            class="dropdown-menu"
-                            aria-labelledby="navbarDropdown">
-                            <li>
-                                <span class="dropdown-item">
-                                    Switch Account
-                                </span>
-                            </li>
-                            <li>
-                                <span class="dropdown-item">
-                                    Manage Account
-                                </span>
-                            </li>
-                            <li>
-                                <hr class="dropdown-divider" />
-                            </li>
-
-                            <li>
-                                <span
-                                    class="dropdown-item"
+                    <ul className="navbar-nav mb-2 me-5 mb-lg-0">
+                        <li className="nav-item dropdown">
+                            <span
+                                className="nav-link dropdown-toggle"
+                                id="navbarDropdown"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                <i
+                                    className="bi bi-person-circle"
+                                    style={{ fontSize: "2em" }}></i>
+                            </span>
+                            <ul
+                                className="dropdown-menu dropdown-menu-end"
+                                aria-labelledby="navbarDropdown">
+                                <li>
+                                    <span className="dropdown-item">
+                                        Switch Account
+                                    </span>
+                                </li>
+                                <li>
+                                    <span className="dropdown-item">
+                                        Manage Account
+                                    </span>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider" />
+                                </li>
+                                <li
                                     onClick={() => {
                                         localStorage.removeItem("token");
                                         localStorage.removeItem("userLoggedIn");
                                         dispatch(setIsLoggedIn(false));
-                                        navigate("/login", { replace: true });
+                                        navigate("/login", {
+                                            replace: true,
+                                        });
                                     }}>
-                                    Log out
-                                </span>
-                            </li>
-                        </ul>
-                    </div>
+                                    <span className="dropdown-item">
+                                        Logout
+                                    </span>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
